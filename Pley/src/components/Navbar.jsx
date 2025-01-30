9dc78c405984aa4ce2c85a775e6705525920c300
Praween Pongpat <praween.pongpat@gmail.com>
2025-01-30 11:15:47 -0500

    adjust icon in dev

--- Pley/src/components/Navbar.jsx
+++ Pley/src/components/Navbar.jsx
@@ -4,6 +4,11 @@ import { Link } from 'react-router-dom';
 import './NavBar.css';
 import './Register';
 import './Profile';
+import { IoLogOut } from "react-icons/io5";
+import { FaHome } from "react-icons/fa";
+import { FaThList } from "react-icons/fa";
+import { MdAccountCircle } from "react-icons/md";
+import { IoStorefront } from "react-icons/io5";
 
 import { useUserContext } from './UserContext';
 
@@ -16,18 +21,18 @@ export default function NavBar() {
       {currentToken === null ? (
         // Links to show when no user is logged in
         <>
-          <Link to="/home" className="nav-link">Home</Link>
-          <Link to="/customer-list" className="nav-link">Customer List</Link>
-          <Link to="/register" className="nav-link">Register</Link>
-          <Link to="/profile" className="nav-link">Profile</Link>
+          <Link to="/home" className="nav-link"><FaHome className='my-icon'/>  Home</Link>
+          <Link to="/customer-list" className="nav-link"><FaThList className='my-icon'/>  Customer List</Link>
+          <Link to="/register" className="nav-link"><MdAccountCircle className='my-icon' />  Register</Link>
+          {/* <Link to="/profile" className="nav-link"><IoStorefront className='my-icon' />  Profile</Link> */}
           
         </>
       ) : (
         <>
-          <Link to="/home" className="nav-link">Home</Link>
-          <Link to="/customer" className="nav-link">Customer</Link>
+          <Link to="/home" className="nav-link"><FaHome className='my-icon'/>  Home</Link>
+          <Link to="/customer-list" className="nav-link"><FaThList className='my-icon'/>  Customer List</Link>
           <Link to="/profile" className="nav-link">Profile</Link>
-          <button onClick={{logout}}>logout?</button>
+          <button onClick={logout}><IoLogOut className='my-icon'/>  logout?</button>
         </>
         
       )}
